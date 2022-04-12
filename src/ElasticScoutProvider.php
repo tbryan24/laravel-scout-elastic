a38e9784a550ac839f12859b36234d53cf4c0501
wangchengfei <wangchengfei@shall-buy.com>
2022-04-12 16:45:48 +0800

    provider

--- src/ElasticScoutProvider.php
+++ src/ElasticScoutProvider.php
@@ -1,14 +1,44 @@
 <?php
-/**
- * @copyright Copyright&copy;2022
- * Notes: ElasticScoutProvider
- * wangchengfei 2022/4/12 16:18：创建ElasticScoutProvider.php
- */
 
 namespace Tbryan24\LaravelScoutElastic;
 
+use Exception;
+use Elasticsearch\ClientBuilder;
+use Laravel\Scout\EngineManager;
+use Illuminate\Support\ServiceProvider;
+use Tamayo\LaravelScoutElastic\Engines\ElasticsearchEngine;
 
-class ElasticScoutProvider
+class ElasticScoutProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->ensureElasticClientIsInstalled();
 
+        resolve(EngineManager::class)->extend('elasticsearch', function () {
+            return new ElasticsearchEngine(
+                ClientBuilder::create()
+                    ->setHosts(config('scout.elasticsearch.hosts'))
+                    ->build()
+            );
+        });
+    }
+
+    /**
+     * Ensure the Elastic API client is installed.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    protected function ensureElasticClientIsInstalled()
+    {
+        if (class_exists(ClientBuilder::class)) {
+            return;
+        }
+
+        throw new Exception('Please install the Elasticsearch PHP client: elasticsearch/elasticsearch.');
+    }
 }
